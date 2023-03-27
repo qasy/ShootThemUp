7f5de81dc04edf6c4ddf93cf80ef5aacce400d56
Qasy <alekseyivanov0705@gmail.com>
2023-03-27 21:06:35 +0300

    Заменил дефолтные классы пауна и чарактера в гейм моде, создал их блюпринт версии. Создал через код компонент камеры для чарактера и в блюпринте ее передвинул.

--- Source/ShootThemUp/Private/STUGameModeBase.cpp
+++ Source/ShootThemUp/Private/STUGameModeBase.cpp
@@ -2,15 +2,16 @@
 
 
 #include "STUGameModeBase.h"
+#include <Player/STUBaseCharacter.h>
+#include <Player/STUPlayerController.h>
+
+ASTUGameModeBase::ASTUGameModeBase()
+{
+    DefaultPawnClass = ASTUBaseCharacter::StaticClass();
+    PlayerControllerClass = ASTUPlayerController::StaticClass();
+}
 
 void ASTUGameModeBase::BeginPlay()
 {
-    int32 Variable = 0;
-    if (Variable < 10)
-    {
-        for (int32 i = 0; i < 10; ++i)
-        {
-            UE_LOG(LogTemp, Display, TEXT("index: %i"), i);
-        }
-    }
+    Super::BeginPlay();
 }
