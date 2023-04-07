dd26406aae2d3023bb35345bd502f1f8e0c51b4f
Qasy <alekseyivanov0705@gmail.com>
2023-04-07 07:14:33 +0300

    Начали реализовывать логику взрыва гранаты. Вернее создаем сферу с помощью дебажных рисовалок из DrawDebugHelpers.h хедера, в пределах которой персонажу будет наноситься урон.

---
+++ Source/ShootThemUp/Public/Dev/STUDevDamageActor.h
@@ -0,0 +1,34 @@
+// Shoot Them Up Game
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "STUDevDamageActor.generated.h"
+
+UCLASS()
+class SHOOTTHEMUP_API ASTUDevDamageActor : public AActor
+{
+    GENERATED_BODY()
+
+  public:
+    // Sets default values for this actor's properties
+    ASTUDevDamageActor();
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+    USceneComponent *SceneComponent;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float Radius = 300.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FColor SphereColor = FColor::Red;
+
+  protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+  public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+};
