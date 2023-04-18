ee510dc8cd97b5dbeb682304c45abb9ca40fadae
Qasy <alekseyivanov0705@gmail.com>
2023-04-18 07:23:58 +0300

    Сделали реализацию отхила. Длеается это с помощью таймера, дескриптор на который типа FTimerHandle оздается в заголовочном файле HealthComponent. После чего, по таймеру вызывается функция обработки отхила. Главнное граммотно прописать логику когда таймер запускается и когда очищается.

--- Source/ShootThemUp/Public/Components/STUHealthComponent.h
+++ Source/ShootThemUp/Public/Components/STUHealthComponent.h
@@ -26,7 +26,7 @@ DECLARE_MULTICAST_DELEGATE_OneParam(FOnHealthChanged, float)
     UFUNCTION(BlueprintCallable)
     bool IsDead() const
     {
-        return (Health <= 0.0f);
+        return FMath::IsNearlyZero(Health);
     }
 
     FOnDeath OnDeath;
@@ -36,13 +36,29 @@ DECLARE_MULTICAST_DELEGATE_OneParam(FOnHealthChanged, float)
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Health", meta = (ClampMin = "0.0", ClampMax = "1000.0"))
     float MaxHealth = 100.0f;
 
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Heal")
+    bool AutoHeal = true;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Heal", meta = (EditCondition = "AutoHeal"))
+    float HealUpdateTime = 1.0f;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Heal", meta = (EditCondition = "AutoHeal"))
+    float HealDelay = 3.0f;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Heal", meta = (EditCondition = "AutoHeal"))
+    float HealModifier = 5.0f;
+
     // Called when the game starts
     virtual void BeginPlay() override;
 
   private:
     float Health = 0.0f;
+    FTimerHandle HealTimerHandle;
 
     UFUNCTION()
     void OnTakeAnyDamageHandle(AActor *DamagedActor, float Damage, const class UDamageType *DamageType,
                                class AController *InstigatedBy, AActor *DamageCauser);
+
+    void HealUpdate();
+    void SetHealth(float NewHealth);
 };
